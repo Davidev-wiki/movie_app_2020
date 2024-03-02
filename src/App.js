fc4bc0f5eb4901f8e480a11dd6a2efc759e6c4f3
Davidev-wiki <accelerator@kakao.com>
2024-03-02 15:04:09 +0900

    Class type Component, state, lifecycle

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from "react";
-import Food, { foodILike } from "./Food";
 
-function App() {
-    return (
-        <div>
-            {foodILike.map(dish => (
-                <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-            ))}
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log('constructor!')
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount')
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate')
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount')
+    }
+
+    state = {
+        count: 0,
+    }
+
+    add = () => {
+        this.setState({ count: this.state.count + 1 })
+    }
+
+    minus = () => {
+        this.setState({ count: this.state.count - 1 })
+    }
+
+    render() {
+        console.log('render')
+        return <div>
+            <h1>The number is: {this.state.count}</h1>
+            <button onClick={this.add}>Add</button>
+            <button onClick={this.minus}>Minus</button>
         </div>
-
-    )
+    }
 }
 
 export default App;
